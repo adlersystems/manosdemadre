da81729f307255ade6f0ace8d1214f64783dc7e4
Adler Systems <rafaguilarsol@gmail.com>
2019-09-25 12:38:40 -0600

    f7

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+// Initialize your app
+var myApp = new Framework7({
+    animateNavBackIcon: true,
+    // Enable templates auto precompilation
+    precompileTemplates: true,
+    // Enabled pages rendering using Template7
+    template7Pages: true,
+    // Specify Template7 data for pages
+    template7Data: {
+    }
+});
+
+
+// Events request
+myApp.onPageInit('eventos.html#eventos', function (page) {  
+	$$.get('http://www.manosdemadre.org/app/eventos.txt', {}, function (data) {        
+		$$('eventos.html#news').html(data);          
+	});
+});
+
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
